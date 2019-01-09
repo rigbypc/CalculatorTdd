23921bb0b5250eeb508548e175a3ba4335f99765
Peter C Rigby <rigbypc@gmail.com>
2019-01-09 15:10:36 -0500

    Test the average

--- CalculatorTdd/src/main/java/calculator/Calculator.java
+++ CalculatorTdd/src/main/java/calculator/Calculator.java
@@ -1,17 +1,30 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
+	ArrayList<Double> values;
+	
 	public Calculator() {
-		
+		values = new ArrayList<Double>();
 	}
 	
 	public void add(Double value) {
+		values.add(value);
 		
 	}
 	
 	public Double average() {
-		return Double.NaN;
+		Double total = 0.0;
+		
+		for (Double value : values) {
+			total += value;
+		}
+		
+		return total/(double)values.size();
+		
+		
 	}
 
 }
--- CalculatorTdd/src/test/java/calculator/TestCalculator.java
+++ CalculatorTdd/src/test/java/calculator/TestCalculator.java
@@ -11,7 +11,7 @@ class TestCalculator {
 		Calculator calculator = new Calculator();
 		
 		calculator.add(1.0);
-		calculator.add(3.0);
+		calculator.add(2.0);
 		calculator.add(1000.0);
 		
 		assertEquals(334.34, calculator.average(), .01);
