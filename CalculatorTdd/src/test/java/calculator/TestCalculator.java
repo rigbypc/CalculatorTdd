ce43e98099cae76786d99df6ca80839760ae3aaf
Peter C Rigby <rigbypc@gmail.com>
2019-01-09 15:25:29 -0500

    Add BeforeEach to reset the calculator each time

--- CalculatorTdd/src/test/java/calculator/TestCalculator.java
+++ CalculatorTdd/src/test/java/calculator/TestCalculator.java
@@ -2,17 +2,24 @@ package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestCalculator {
 
-	@Test
-	void testAverage() {
-		Calculator calculator = new Calculator();
+	Calculator calculator;
+	
+	@BeforeEach
+	void setupCalculator() {
+		calculator = new Calculator();
 		
 		calculator.add(1.0);
 		calculator.add(2.0);
 		calculator.add(1000.0);
+	}
+	
+	@Test
+	void testAverage() {
 		
 		try {
 			assertEquals(334.34, calculator.average(), .01);
@@ -40,12 +47,6 @@ class TestCalculator {
 	
 	@Test
 	void testMedian() {
-		
-		Calculator calculator = new Calculator();
-		
-		calculator.add(1.0);
-		calculator.add(2.0);
-		calculator.add(1000.0);
 		
 		assertEquals(2.0, calculator.median(), 0.01);
 		
